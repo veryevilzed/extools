98ba1a4f1ec261e444df6b9dd53cef015e05d767
Dmitry Vysochin <dmitry.vysochin@gmail.com>
2014-05-22 12:05:18 +0400

    Fix

--- lib/extools.ex
+++ lib/extools.ex
@@ -8,23 +8,16 @@ defmodule ExTools do
 
     defp to_map(list), do: list
 
-    def parse(text) do
-        case :etoml.parse(text) do
-            {:ok, toml} -> {:ok, to_map(toml) }
-            err -> err
-        end
-    end
-
     @doc """
     Переводит Dict старого стиля в Map
     """
-    def dict_to_map(dict), do: to_map(toml)
+    def dict_to_map(dict), do: to_map(dict)
 
-    def binary_to_atom(bin) when is_atom(bin), do: bin
-    def binary_to_atom(bin) when is_binary(bin), do: binary_to_atom(bin)
+    def bin_to_atom(bin) when is_atom(bin), do: bin
+    def bin_to_atom(bin) when is_binary(bin), do: Kernel.binary_to_atom(bin)
 
-    def binary_to_list(bin) when is_list(bin), do: bin
-    def binary_to_list(bin) when is_binary(bin), do: :erlang.binary_to_list(bin)
+    def bin_to_list(bin) when is_list(bin), do: bin
+    def bin_to_list(bin) when is_binary(bin), do: :erlang.binary_to_list(bin)
 
 
 end
